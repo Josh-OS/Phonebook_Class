72e5046aafb276a61a678ccc1026f8d9f3e5f753
Josh-OS <skybyte4me@gmail.com>
2019-04-03 17:09:33 +0800

    first work

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+include_once('phonelib.php');
+?>
+<!DOCTYPE Html>
+
+<html>
+<head>
+<title>Phonebook</title>
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
+
+<body>
+
+<div id="bookcontainer">
+
+<div id="book">
+<table class="entry">
+	<thead>
+		<tr>
+		<th></th>
+		<th style="background-color: yellow;">Firstname</th>
+		<th>Lastname</th>
+		<th>Gender</th>
+		<th>Birthdate</th>
+		<th>Number</th>
+		</tr>
+	<thead>
+
+
+	<!--thi is foreach-->
+	<tbody>
+		<tr>
+		<td class="tllist"></th>
+		<td class="tllist">Jesson</td>
+		<td class="tllist">Jamola</td>
+		<td class="tllist">Male</td>
+		<td class="tllist">October</td>
+		<td class="tllist">d09382637283</td>
+		</tr>
+	</tbody>
+</div>
+
+</div>
+
+
+
+
+
+</body>
+</html>
