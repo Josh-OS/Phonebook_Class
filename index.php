78cfcf793ac97f763ae5d206e8204f6ccd146464
Josh-OS <skybyte4me@gmail.com>
2019-04-11 17:35:40 +0800

    Phonebook contact style is slice - FULL

--- index.php
+++ index.php
@@ -43,50 +43,47 @@ if($_GET['frmEdit_id']) {
 		<div id="bookcontainer">
 
 			<div id="book">
-				<table class="contactlist">
-					<tr>
-						<th>Firstname</th>
-						<th>Lastname</th>
-						<th>Gender</th>
-						<th>Birthdate</th>
-						<th>Number</th>
-					</tr>
-				</table>
+				<span class="spanSlice_head">			
+						<span class="spanSlice_head_fname"><strong>Firstname</strong></span>
+						<span class="spanSlice_head_lname"><strong>Lastname</strong></span>
+						<span class="spanSlice_head_gender"><strong>Gender</strong></span>
+						<span class="spanSlice_head_bday"><strong>Birthdate</strong></span>
+						<span class="spanSlice_head_num"><strong>Number</strong></span>
+				</span>
 
 
 					
 						<!--this is foreach-->
 						<?php foreach(explode("\n", $csvops->fetchcsv()) as $fetchedLine): ?>
-							<?php echo $csvops->csv_contents; ?> <!-- This is  test -->
 							<?php [$fl_fname, $fl_lname, $fl_gender, $fl_birthdate, $fl_number, $fl_id] = explode(',', $fetchedLine); ?>
-		
-							<span class="spanSlice">
 								
-									<span class="spanSlice_colmn_fname"><a class="tllink" href="?view=<?php echo $fl_id; ?>"><?php echo "$fl_fname"; ?></a></span>
+							<?php if ($fl_id == NULL) break; ?> <!-- // REMOVE EXCESS EMPTY SLICE BEFORE USER INPUT SLICE -->
+							
+							<a class="tllink" href="?view=<?php echo $fl_id; ?>">
+								<span class="spanSlice">
+								
+									<span class="spanSlice_colmn_fname"><?php echo "$fl_fname"; ?></span>
 									<span class="spanSlice_colmn_lname"><?php echo "$fl_lname"; ?></span>
 									<span class="spanSlice_colmn_gender"><?php echo "$fl_gender"; ?></span>
 									<span class="spanSlice_colmn_bday"><?php echo "$fl_birthdate"; ?></span>
 									<span class="spanSlice_colmn_num"><?php echo "$fl_number"; ?></span>
-							
-							</span>
+								</span>
+							</a>
 	
 						<?php endforeach; ?>
-
 						
-						<table class="contactlist">
-						<tr>
+						<span class="spanSlice">
 							<form action="" method="GET">
-								<td class="tlItem"><input class="frmInput" type="text" name="frm_fname" placeholder="Firstname"></td>
-								<td class="tlItem"><input class="frmInput" type="text" name="frm_lname" placeholder="Lastname"></td>
-								<td class="tlItem"><select class="frmInput" name="frm_gender">
-								<option value="Male">Male</option>
-								<option value="Female">Female</option></td>
-								<td class="tlItem"><input class="frmInput" type="date" name="frm_birthdate" placeholder="Birthdate"></td>
-								<td class="tlItem"><input class="frmInput" type="text" name="frm_number" placeholder="Mobile number"></td>
+								<span class="spanSlice_colmn_fname"><input class="frmInput" type="text" name="frm_fname" placeholder="Firstname"></span>
+								<span class="spanSlice_colmn_lname"><input class="frmInput" type="text" name="frm_lname" placeholder="Lastname"></span>
+								<span class="spanSlice_colmn_gender"><select class="frmInput" name="frm_gender">
+									<option value="Male">Male</option>
+									<option value="Female">Female</option></select></span>
+								<span class="spanSlice_colmn_bday"><input class="frmInput" type="date" name="frm_birthdate" placeholder="Birthdate"></span>
+								<span class="spanSlice_colmn_num"><input class="frmInput" type="text" name="frm_number" placeholder="Mobile number"></span>
 								<input class="frmInput" type="submit"/>
 							</form>
-						</tr>
-						</table>
+						</span>
 			</div>
 
 		</div>
