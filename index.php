b893183801ef7a697d1b3ddb6f5ba50d686cf2d2
Josh-OS <skybyte4me@gmail.com>
2019-04-11 15:40:53 +0800

    Slice contact list separation

--- index.php
+++ index.php
@@ -43,36 +43,37 @@ if($_GET['frmEdit_id']) {
 		<div id="bookcontainer">
 
 			<div id="book">
-				<table class="entry">
-					<thead>
-						<tr>
-							<th>Firstname</th>
-							<th>Lastname</th>
-							<th>Gender</th>
-							<th>Birthdate</th>
-							<th>Number</th>
-						</tr>
-					<thead>
+				<table class="contactlist">
+					<tr>
+						<th>Firstname</th>
+						<th>Lastname</th>
+						<th>Gender</th>
+						<th>Birthdate</th>
+						<th>Number</th>
+					</tr>
+				</table>
 
 
-					<tbody>
-						<!--thi is foreach-->
+					
+						<!--this is foreach-->
 						<?php foreach(explode("\n", $csvops->fetchcsv()) as $fetchedLine): ?>
 							<?php echo $csvops->csv_contents; ?> <!-- This is  test -->
 							<?php [$fl_fname, $fl_lname, $fl_gender, $fl_birthdate, $fl_number, $fl_id] = explode(',', $fetchedLine); ?>
 		
-							<div color="red">
-								<tr>
-									<td class="tlItem"><a class="tllink" href="?view=<?php echo $fl_id; ?>"><?php echo "$fl_fname"; ?></a></td>
-									<td class="tlItem"><?php echo "$fl_lname"; ?></td>
-									<td class="tlItem"><?php echo "$fl_gender"; ?></td>
-									<td class="tlItem"><?php echo "$fl_birthdate"; ?></td>
-									<td class="tlItem"><?php echo "$fl_number"; ?></td>
-								</tr>
-							</div>
+							<span class="spanSlice">
+								
+									<span class="spanSlice_colmn_fname"><a class="tllink" href="?view=<?php echo $fl_id; ?>"><?php echo "$fl_fname"; ?></a></span>
+									<span class="spanSlice_colmn_lname"><?php echo "$fl_lname"; ?></span>
+									<span class="spanSlice_colmn_gender"><?php echo "$fl_gender"; ?></span>
+									<span class="spanSlice_colmn_bday"><?php echo "$fl_birthdate"; ?></span>
+									<span class="spanSlice_colmn_num"><?php echo "$fl_number"; ?></span>
+							
+							</span>
 	
 						<?php endforeach; ?>
-		
+
+						
+						<table class="contactlist">
 						<tr>
 							<form action="" method="GET">
 								<td class="tlItem"><input class="frmInput" type="text" name="frm_fname" placeholder="Firstname"></td>
@@ -85,7 +86,7 @@ if($_GET['frmEdit_id']) {
 								<input class="frmInput" type="submit"/>
 							</form>
 						</tr>
-					</tbody>
+						</table>
 			</div>
 
 		</div>
